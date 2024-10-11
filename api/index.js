5ea61bed5ce143bc5aaf887bc2f48dea933ead43
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-10-11 16:11:31 +0000

    Create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use(express.json());
 
 // Use user routes
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 // Start server
 app.listen(3000, () => {
