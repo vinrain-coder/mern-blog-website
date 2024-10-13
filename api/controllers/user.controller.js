406557c044fc9eeac2fecc683f8edf29f0c6f84d
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-10-13 10:11:00 +0000

    Created update-user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,67 @@
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error";
+import User from "../models/user.model";
+
 export const test = (req, res) => {
   res.json({ message: "Api is working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  // Check if the logged-in user is allowed to update this account
+  if (req.user._id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
+  }
+
+  // Password validation and hashing
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, "Password must be at least 6 characters"));
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+
+  // Username validation
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "Username must be between 7 and 20 characters")
+      );
+    }
+
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
+    }
+
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username must be lowercase"));
+    }
+
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "Username can only contain letters and numbers")
+      );
+    }
+  }
+
+  try {
+    // Update the user with the provided details
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true } // Return the updated user
+    );
+
+    // Exclude password from the response
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cookieParser());
 
 // Use user routes
 app.use("/api/user", userRoutes);
