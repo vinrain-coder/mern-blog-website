638d89793e558b6b935df6c2d16f573ca8526f9b
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-10-13 22:22:27 +0000

    Added create post api route

---
+++ api/models/post.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userid: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default: "https://images.app.goo.gl/sjYBtCjfyP695bpW7",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("post", postSchema);
+
+export default Post;
