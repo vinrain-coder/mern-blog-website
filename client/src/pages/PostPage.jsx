ab9fb7ab36e3561227baa7e940d0448c8cae3adb
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-10-14 22:36:00 +0000

    Pending changes exported from your codespace

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -72,6 +73,7 @@ export default function PostPage() {
       <div className="max-w-4xl mx-auto w-full">
         <CallToAction/>
       </div>
+      <CommentSection postId={post._id}/>
     </main>
   );
 }
