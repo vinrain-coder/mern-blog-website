5be6ff974a300c4dfa90c8b66ca3a826534d2b39
vinrain-coder <vincentombogo57@gmail.com>
2024-10-15 21:21:20 +0000

    update post and user controllers

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,26 +1,23 @@
-import Post from "../models/post.model.js";
-import { errorHandler } from "../utils/error.js";
+import Post from '../models/post.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
-    return next(errorHandler(403, "You are not allowed to create a post"));
+    return next(errorHandler(403, 'You are not allowed to create a post'));
   }
   if (!req.body.title || !req.body.content) {
-    return next(errorHandler(400, "Please provide all required fields"));
+    return next(errorHandler(400, 'Please provide all required fields'));
   }
   const slug = req.body.title
-    .toLowerCase()                      // Convert to lowercase
-    .replace(/\s+/g, "-")               // Replace spaces with hyphens
-    .replace(/[^a-z0-9-]/g, "")         // Remove non-alphanumeric characters except hyphens
-    .replace(/--+/g, "-")               // Replace multiple hyphens with a single one
-    .trim();                            // Remove leading or trailing spaces
-
+    .split(' ')
+    .join('-')
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, '');
   const newPost = new Post({
     ...req.body,
     slug,
     userId: req.user.id,
   });
-
   try {
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
@@ -33,7 +30,7 @@ export const getposts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
     const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
@@ -41,8 +38,8 @@ export const getposts = async (req, res, next) => {
       ...(req.query.postId && { _id: req.query.postId }),
       ...(req.query.searchTerm && {
         $or: [
-          { title: { $regex: req.query.searchTerm, $options: "i" } },
-          { content: { $regex: req.query.searchTerm, $options: "i" } },
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
         ],
       }),
     })
@@ -76,16 +73,15 @@ export const getposts = async (req, res, next) => {
 
 export const deletepost = async (req, res, next) => {
   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-    return next(errorHandler(403, "You are not allowed to delete this post"));
+    return next(errorHandler(403, 'You are not allowed to delete this post'));
   }
   try {
     await Post.findByIdAndDelete(req.params.postId);
-    res.status(200).json("The post has been deleted");
+    res.status(200).json('The post has been deleted');
   } catch (error) {
     next(error);
   }
 };
-
 
 export const updatepost = async (req, res, next) => {
   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,29 +8,29 @@ export const test = (req, res) => {
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, "You are not allowed to update this user"));
+    return next(errorHandler(403, 'You are not allowed to update this user'));
   }
   if (req.body.password) {
     if (req.body.password.length < 6) {
-      return next(errorHandler(400, "Password must be at least 6 characters"));
+      return next(errorHandler(400, 'Password must be at least 6 characters'));
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
   if (req.body.username) {
     if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
-        errorHandler(400, "Username must be between 7 and 20 characters")
+        errorHandler(400, 'Username must be between 7 and 20 characters')
       );
     }
-    if (req.body.username.includes(" ")) {
-      return next(errorHandler(400, "Username cannot contain spaces"));
+    if (req.body.username.includes(' ')) {
+      return next(errorHandler(400, 'Username cannot contain spaces'));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
-      return next(errorHandler(400, "Username must be lowercase"));
+      return next(errorHandler(400, 'Username must be lowercase'));
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(
-        errorHandler(400, "Username can only contain letters and numbers")
+        errorHandler(400, 'Username can only contain letters and numbers')
       );
     }
   }
