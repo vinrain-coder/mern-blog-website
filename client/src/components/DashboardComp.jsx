a3d958fe38af4ba018040e87658d3623c99a7c44
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-10-19 09:18:37 +0300

    Update DashboardComp.jsx

--- client/src/components/DashboardComp.jsx
+++ client/src/components/DashboardComp.jsx
@@ -69,7 +69,7 @@ export default function DashboardComp() {
   return (
     <div className='p-3 md:mx-auto'>
       <div className='flex-wrap flex gap-4 justify-center'>
-        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 md:w-72 w-full rounded-md shadow-md'>
+        <div className='flex flex-col p-3 dark:bg-slate-800 gap-4 sm:w-[70px] w-full rounded-md shadow-md'>
           <div className='flex justify-between'>
             <div className=''>
               <h3 className='text-gray-500 text-md uppercase'>Total Users</h3>
